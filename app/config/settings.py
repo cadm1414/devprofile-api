e5ef3c40aa3b867acf86512a7a23589a9b4bc2ca
cadm1414 <calitos0410@gmail.com>
2025-09-02 01:11:02 -0500

    fix: driver 17 pra conexion base de datos

--- app/config/settings.py
+++ app/config/settings.py
@@ -25,10 +25,11 @@ class Settings(BaseSettings):
         if self.DB_USER and self.DB_PASSWORD:
             password = urllib.parse.quote_plus(self.DB_PASSWORD)            
             user = urllib.parse.quote_plus(self.DB_USER)
-                        
-            driver = "ODBC+Driver+18+for+SQL+Server" if os.getenv("FLY_APP_NAME") else self.DB_DRIVER
-            
-            return f"mssql+pyodbc://{user}:{password}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?driver={driver}&Encrypt=no&TrustServerCertificate=yes"
+                                   
+            if os.getenv("FLY_APP_NAME"):
+                return f"mssql+pyodbc://{user}:{password}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=no&TrustServerCertificate=yes&Connection+Timeout=30"
+            else:                
+                return f"mssql+pyodbc://{user}:{password}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?driver={self.DB_DRIVER}&Encrypt=yes&TrustServerCertificate=yes"
         
         return f"mssql+pyodbc://{self.DB_HOST}/{self.DB_NAME}?driver={self.DB_DRIVER}&{self.DB_AUTH}"
     
